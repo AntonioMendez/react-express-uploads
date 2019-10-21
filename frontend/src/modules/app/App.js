f15544ccb8a9855fcbc02ab68b8f1b06409f8321
Antonio <amensa.software@gmail.com>
2019-10-21 20:51:40 +0100

    update App.js indent

--- frontend/src/modules/app/App.js
+++ frontend/src/modules/app/App.js
@@ -4,34 +4,34 @@ import CloudDownload from '@material-ui/icons/CloudDownload'
 import './App.css'
 
 class App extends React.Component {
-	onClick = () => {
-		let options = {
-			method: 'POST',
-			body: JSON.stringify({ requestId: '53', bundle: false }),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		}
+  onClick = () => {
+    let options = {
+      method: 'POST',
+      body: JSON.stringify({ requestId: '53', bundle: false }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    
+    fetch('/getDownloadURL', options)
+      .then(response => response.json())
+      .then(res => {
+        console.log(res)
+        if (res.success) window.location.href = res.url
+        else alert(res.error)
+       })
+  }
 
-		fetch('/getDownloadURL', options)
-			.then(response => response.json())
-			.then(res => {
-				console.log(res)
-				if (res.success) window.location.href = res.url
-				else alert(res.error)
-			})
-	}
-
-	render() {
-		return (
-			<div id='main-container'>
-				<Button variant='contained' color='primary' className='download-button' onClick={this.onClick}>
-					<CloudDownload />
-					Download file
-				</Button>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div id='main-container'>
+        <Button variant='contained' color='primary' className='download-button' onClick={this.onClick}>
+          <CloudDownload />
+            Download file
+        </Button>
+      </div>
+    )
+  }
 }
 
 export default App
